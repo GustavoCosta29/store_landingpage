4831614e8d6c555494a1b8ad4178581d440d5385
Gustavo Costa <gucostapt@gmail.com>
2022-09-13 19:28:34 +0100

    add feature form validation and bug fix

--- js/script.js
+++ js/script.js
@@ -1,5 +1,73 @@
+// form validation
+function formValidateDados() {
+    // value of the form element
+    let name = document.getElementById('name').value; 
+    let email = document.getElementById('email').value;
+    let cpf = document.getElementById('cpf').value;
+    let gender1 = document.querySelector('#gender1').checked 
+    let gender2 = document.querySelector('#gender2').checked 
+    let gender3 = document.querySelector('#gender3').checked
+    let txtName = document.getElementById('txtName')
+    let txtEmail = document.getElementById('txtEmail')
+    let txtCpf = document.getElementById('txtCpf')
+    let txtGenero = document.getElementById('txtGenero')    
+
+    //variable of control
+    let validation =  0
+    
+    // name validation 
+    if (name.length > 0) {
+      txtName.innerText = ('ok')
+      txtName.classList.remove('spanValidationError')  
+      validation++      
+    }else{    
+        txtName.innerText = ('* Preencher o campo corretamente!')
+        txtName.classList.add('spanValidationError')  
+    }
+
+    // email validation
+    if (email.indexOf('@') > -1 && email.indexOf('.') > -1 )  {
+        txtEmail.innerText = ('ok')
+        txtEmail.classList.remove('spanValidationError')   
+        validation++ 
+    }else{
+        txtEmail.innerText = ('* Preencher o campo corretamente!')
+        txtEmail.classList.add('spanValidationError') 
+    }
+
+    // cpf validation
+    if (cpf.length === 11) {
+        txtCpf.innerText = ('ok')
+        txtCpf.classList.remove('spanValidationError') 
+        validation++      
+    }else{    
+        txtCpf.innerText  = ('* Preencher o campo corretamente!')
+        txtCpf.classList.add('spanValidationError') 
+    }
+
+    // gender validation
+    if (gender1 != false || gender2 != false || gender3 != false) {
+        txtGenero.innerText = ('ok')
+        txtGenero.classList.remove('spanValidationError')
+        validation++
+    }else{
+        txtGenero.innerText  = ('*')
+        txtGenero.classList.add('spanValidationError') 
+    }  
+
+    // control variable validation and form submission 
+    if (validation === 4) { 
+        alert('Form accepted with success!')
+        // frmCadastro.submit()      
+    }else{
+        console.log('error')
+    }
+}   
+
+//variable of control
 let page = 0
 
+// fetch of API method GET
 fetch('https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=1')
     .then(response => { response.json()
     .then(listProducts)
@@ -8,21 +76,12 @@ fetch('https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?pag
         console.log(error)
 })    
 
-
-    
-    
-
-
-
-
+// listing products coming from API
 function listProducts(objeto) {
     let textHtml = document.querySelector('#section_two')
     let produto = objeto.products.length
     const btn = document.querySelector('#buttonMoreProducts')
-    
 
-    
-    
     if( page === 3) {
         btn.classList.add('hiddenButton')    
     }else{
@@ -37,32 +96,59 @@ function listProducts(objeto) {
             <span>ou ${objeto.products[c].installments.count}x de R$${(objeto.products[c].installments.value).toFixed(2).replace('.',',')} </span>
             <button type="menu">Comprar</button>       
             </div> 
-            `  
-              
+            `                
         }
         page ++
         console.log(page)
-    }
-    
-    
-    
-    
-
-    
+    }  
 }
 
-
-
-
+// fetch of API method GET next page
 async function seeMoreProducts() {
-
-
-
-
-await fetch(`https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=${page}`)
+    await fetch(`https://frontend-intern-challenge-api.iurykrieger.vercel.app/products?page=${page}`)
     .then((response) => response.json())
-    .then(listProducts)
-    
-}
+    .then(listProducts)       
+}   
 
-/* pagination e index */ 
+// new friends form validation
+function formValidateEmail() {
+    // value of the form element
+    let nameFriend = document.getElementById('nameFriend').value
+    let emailFriend = document.getElementById('emailFriend').value
+    let txtNameFriend = document.getElementById('txtNameFriend')
+    let txtEmailFriend = document.getElementById('txtEmailFriend')
+
+    //variable of control
+    let validationFormEmail = 0
+
+    // name validation
+    if (nameFriend.length > 0) {
+        txtNameFriend.innerText = ('ok')
+        txtNameFriend.classList.remove('spanValidationError') 
+        
+        validationFormEmail++      
+    }else{    
+        txtNameFriend.innerText = ('* Preencher o campo corretamente!')
+        txtNameFriend.classList.add('spanValidationError')  
+    }
+
+    // email validation
+    if (emailFriend.indexOf('@') > -1 && emailFriend.indexOf('.') > -1 )  {
+        txtEmailFriend.innerText = ('ok')
+        txtEmailFriend.classList.remove('spanValidationError')
+            
+        validationFormEmail++ 
+    }else{
+        txtEmailFriend.innerText = ('* Preencher o campo corretamente!')
+        txtEmailFriend.classList.add('spanValidationError') 
+    }
+
+    // control variable validation and form submission
+    if (validationFormEmail === 2) { 
+        alert('form accepted!')
+        // form_section_three.submit()
+
+    }else {
+        console.log('error')
+    }
+}
